f205c0ddc61dc4d7513481645408e86cfa97717c
kevin <kevin@mythica.ai>
2025-07-07 13:53:20 -0700

    Update raw exporter to match GLB separate primatives per material

--- blender_scenetalk/export_collection_rollbound.py
+++ blender_scenetalk/export_collection_rollbound.py
@@ -1,7 +1,7 @@
 import bpy
 from pydantic import BaseModel
 from .async_wrap import run_async_bg
-from .export_mesh_simple import export_mesh_simple
+from .export_mesh_scenetalk import export_mesh_scenetalk
 
 from .host.app import session_manager
 from .host.models import GeometrySet, GeometryFlat
@@ -11,27 +11,6 @@ from .host.ops import Ops
 class GeometryOp(BaseModel):
     op: Ops = Ops.GEOMETRY
     data: GeometrySet
-
-
-def encode_scenetalk_geometry(mesh_data: dict) -> GeometryFlat:
-    # Flatten indices from list of lists to single list, tessellating quads into triangles
-    flat_indices = []
-    for face_indices in mesh_data['indices']:
-        if len(face_indices) == 3:
-            flat_indices.extend(face_indices)
-        elif len(face_indices) == 4:
-            flat_indices.extend([face_indices[0], face_indices[1], face_indices[2]])
-            flat_indices.extend([face_indices[0], face_indices[2], face_indices[3]])
-        else:
-            print(f"Warning: Unsupported face type: {len(face_indices)}")
-
-    return GeometryFlat(
-        points=mesh_data['vertices'],
-        normals=mesh_data['normals'],
-        uvs=mesh_data['uvs'],
-        colors=mesh_data['colors'],
-        indices=flat_indices
-    )
 
 
 def export_collection_rollbound():
@@ -60,8 +39,9 @@ def export_collection_rollbound():
         for obj in collection.objects:
             if obj.type != 'MESH':
                 continue
-            mesh_data = export_mesh_simple(obj.name, obj)
-            geometry.geometry[obj.name] = encode_scenetalk_geometry(mesh_data)
+
+            object_geometry = export_mesh_scenetalk(obj.name, obj)
+            geometry.geometry.update(object_geometry.geometry)
 
         # Send geometry to clients
         geometry_op = GeometryOp(data=geometry)
