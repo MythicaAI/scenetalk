32049ef654841a02a452fd452344170b37ecfff1
kevin <kevin@mythica.ai>
2025-07-07 10:40:40 -0700

    Fix data format issues

--- blender_scenetalk/export_collection_rollbound.py
+++ blender_scenetalk/export_collection_rollbound.py
@@ -4,13 +4,34 @@ from .async_wrap import run_async_bg
 from .export_mesh_simple import export_mesh_simple
 
 from .host.app import session_manager
-from .host.models import GeometrySet
+from .host.models import GeometrySet, GeometryFlat
 from .host.ops import Ops
 
 
 class GeometryOp(BaseModel):
     op: Ops = Ops.GEOMETRY
     data: GeometrySet
+
+
+def encode_scenetalk_geometry(mesh_data: dict) -> GeometryFlat:
+    # Flatten indices from list of lists to single list, tessellating quads into triangles
+    flat_indices = []
+    for face_indices in mesh_data['indices']:
+        if len(face_indices) == 3:
+            flat_indices.extend(face_indices)
+        elif len(face_indices) == 4:
+            flat_indices.extend([face_indices[0], face_indices[1], face_indices[2]])
+            flat_indices.extend([face_indices[0], face_indices[2], face_indices[3]])
+        else:
+            print(f"Warning: Unsupported face type: {len(face_indices)}")
+
+    return GeometryFlat(
+        points=mesh_data['vertices'],
+        normals=mesh_data['normals'],
+        uvs=mesh_data['uvs'],
+        colors=mesh_data['colors'],
+        indices=flat_indices
+    )
 
 
 def export_collection_rollbound():
@@ -39,8 +60,8 @@ def export_collection_rollbound():
         for obj in collection.objects:
             if obj.type != 'MESH':
                 continue
-
-            geometry.geometry[obj.name] = export_mesh_simple(obj.name, obj)
+            mesh_data = export_mesh_simple(obj.name, obj)
+            geometry.geometry[obj.name] = encode_scenetalk_geometry(mesh_data)
 
         # Send geometry to clients
         geometry_op = GeometryOp(data=geometry)
